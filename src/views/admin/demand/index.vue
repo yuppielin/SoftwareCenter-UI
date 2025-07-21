c8fdd37376817f74bbe81196afee554c8b3dba59
yuppielin <1073096711@qq.com>
2025-07-21 17:53:40 +0800

    将管理员用户的”需求“字眼改成“反馈”

--- src/views/admin/demand/index.vue
+++ src/views/admin/demand/index.vue
@@ -2,18 +2,18 @@
 <div class="app-container">
     <div class="cur-top-admin">
         <img class="icon"  src="@/assets/index/demandManage.png" />
-        <span class="title" >需求管理</span>
+        <span class="title" >软件运营反馈管理</span>
     </div>
   <el-card>
     <!--需求导入对话框--->
-    <el-dialog v-el-drag-dialog :visible.sync="dialogVisible" title="需求导入" width="30%" @dragDialog="handleDrag">
+    <el-dialog v-el-drag-dialog :visible.sync="dialogVisible" title="运营反馈导入" width="30%" @dragDialog="handleDrag">
       <el-row :gutter="10">
         <el-col :span="12">
           <el-button type="primary" size="small" style="background:#1e7d34;border:0" @click="importDemand">确定</el-button>
           <el-button size="small" style="background:#1e7d34;color:white;border:0" @click="dialogVisible=false">取消</el-button>
         </el-col>
         <el-col :span="12" style="text-align:right;">
-          <el-button type="text" style="color:blue;margin-right:10px;" @click="downloadDemandTemplate">需求模板下载</el-button>
+          <el-button type="text" style="color:blue;margin-right:10px;" @click="downloadDemandTemplate">反馈模板下载</el-button>
         </el-col>
       </el-row>
       <el-upload
@@ -40,21 +40,21 @@
       </el-upload>
     </el-dialog>
     <el-tabs v-model="activeName" @tab-click="getDemandQuery">
-      <el-tab-pane name="first" label="新增需求">
+      <el-tab-pane name="first" label="新增反馈">
         <div style="margin-bottom:10px;">
           <el-row :gutter="10">
             <el-col :span="10" style="text-align:left">
               <!-- <el-button v-show="userType=='admin'||userType=='XTUser'" size="small" style="color:white;background:#1e7d34;border:0" icon="el-icon-edit-outline" type="warning" @click="demandCF">需求拆分</el-button>
               <el-button v-show="userType=='admin'||userType=='XTUser'" size="small" style="color:white;background:#1e7d34;border:0" icon="el-icon-edit-outline" type="warning" @click="demandZB">需求整编</el-button> -->
               <!-- <el-button size="small" type="primary" icon="el-icon-upload2" style="background: #1e7d34;border:0;height:32px" @click="dialogVisible=true" :style="{'margin-left':userType=='admin'||userType=='XTUser'?'':0}">需求导入</el-button> -->
-              <el-button :loading="loadingExport" size="small" type="primary" icon="el-icon-download" style="background: #1e7d34;border:0;height:32px" @click="exportDemand">需求导出</el-button>
+              <el-button :loading="loadingExport" size="small" type="primary" icon="el-icon-download" style="background: #1e7d34;border:0;height:32px" @click="exportDemand">运营反馈导出</el-button>
             </el-col>
             <el-col :span="14" style="text-align:right">
               <el-select
                 v-model="search.status"
                 size="small"
                 style="width:120px;"
-                placeholder="需求状态"
+                placeholder="反馈状态"
                 filterable
                 remote
                 reserve-keyword
@@ -67,7 +67,7 @@
                 v-model="search.category"
                 size="small"
                 style="width:120px;margin-left:10px;"
-                placeholder="需求类型"
+                placeholder="反馈类型"
                 filterable
                 remote
                 reserve-keyword
@@ -87,7 +87,7 @@
         </div>
         <el-table :data="demandData" size="mini" stripe :header-cell-style="{background:'#2BB56E',height:'30px'}" style="height:calc(100vh - 310px);overflow-y: auto;">
           <el-table-column width="55" type="index" align="center" label="序号" />
-          <el-table-column align="center" label="需求标题" header-align="center" prop="title" show-overflow-tooltip />
+          <el-table-column align="center" label="反馈标题" header-align="center" prop="title" show-overflow-tooltip />
           <el-table-column align="center" label="软件名称" header-align="center" show-overflow-tooltip width="150">
             <template slot-scope="{row}">
               <span style="color:rgb(8, 151, 151)">
@@ -100,7 +100,7 @@
               {{ row.softwareVersion }}
             </template>
           </el-table-column>
-          <el-table-column align="center" show-overflow-tooltip label="需求类型" width="80">
+            <el-table-column align="center" show-overflow-tooltip label="反馈类型" width="80">
             <template slot-scope="{row}">
               <el-tag 
                 :type="getTagType(row.category)" 
@@ -409,7 +409,7 @@ export default {
       demand.exportDemand(userId, this.search.keyword, this.search.status, zbStatus).then(response => {
         // axios.get(demand.exportDemand(),{responseType: 'arraybuffer'}).then(response => {
         console.log(response, 'response')
-        const fileName = '需求表.xlsx'
+        const fileName = '运营反馈表.xlsx'
         const blob = new Blob([response])
         download(blob, fileName, fileName.lastIndexOf('.') + 1)
         this.loadingExport = false
@@ -504,7 +504,7 @@ export default {
     },
     // 删除需求
     deleteDemand(item) {
-      this.$confirm('确定删除该需求吗？', '提示', {
+      this.$confirm('确定删除该反馈吗？', '提示', {
         type: 'warning'
       }).then(async(e) => {
         await demand.deleteDemand(item.id)
